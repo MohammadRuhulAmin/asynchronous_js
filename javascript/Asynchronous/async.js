bfc49166dc4da7c1a042b5df9a42a3b1431c3f39
Mohammad Ruhul Amin <rA.01403288711@gmail.com>
2022-11-21 00:10:47 +0600

    callback hell with asynchronous

--- javascript/Asynchronous/async.js
+++ javascript/Asynchronous/async.js
@@ -1,32 +1,44 @@
-const taskOne = ()=>{
+const taskOne = (callback)=>{
     setTimeout(()=>{
         console.log("Task One");
+        callback();
     },3000)
 }
 
 
-const taskTwo = ()=>{
+const taskTwo = (callback)=>{
     setTimeout(()=>{
         console.log("Task Two");
+        callback();
     },0)
 }
 
 
-const taskThree = ()=>{
+const taskThree = (callback)=>{
     setTimeout(()=>{
         console.log("Task Three");
+        callback();
     },8000)
 }
 
 const taskFour = ()=>{
     setTimeout(()=>{
         console.log("Task Four");
+        
     },5000)
 }
 
+taskOne(()=>{
+    taskTwo(()=>{
+        taskThree(()=>{
+            taskFour();
+        });
+    });
+});
 
 
-taskOne();
-taskTwo();
-taskThree();
-taskFour();
+
+// taskOne();
+// taskTwo();
+// taskThree();
+// taskFour();
